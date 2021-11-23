75c07cd97ad66c10d4eab19afee04140fa1bfd47
YadavGulshan <yadavgulshan542@gmail.com>
2021-11-23 22:57:02 +0530

    tried verification

--- lib/src/screens/homepage.dart
+++ lib/src/screens/homepage.dart
@@ -1,6 +1,4 @@
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:fitness_app/src/settings/settings_view.dart';
-import 'package:fitness_app/src/theme/theme.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
@@ -11,20 +9,6 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(MyTheme.appName),
-        actions: [
-          IconButton(
-            icon: const Icon(Icons.settings),
-            onPressed: () {
-              // Navigate to the settings page. If the user leaves and returns
-              // to the app after it has been killed while running in the
-              // background, the navigation stack is restored.
-              Navigator.restorablePushNamed(context, SettingsView.routeName);
-            },
-          ),
-        ],
-      ),
       body: Column(
         children: [
           Center(
@@ -38,7 +22,12 @@ class HomePage extends StatelessWidget {
               onPressed: () {
                 FirebaseAuth.instance.signOut();
               },
-              child: const Text('Log Out'))
+              child: const Text('Log Out')),
+          TextButton(
+              onPressed: () {
+                FirebaseAuth.instance.currentUser?.sendEmailVerification();
+              },
+              child: const Text('Send Email Verification')),
         ],
       ),
     );
