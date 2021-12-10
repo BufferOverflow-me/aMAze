2ae0a1e218baa5d0769b1538153a5feccde15080
YadavGulshan <yadavgulshan542@gmail.com>
2021-12-10 20:14:06 +0530

    Made few changes in UI and fixed graddle issue

--- lib/src/screens/homepage.dart
+++ lib/src/screens/homepage.dart
@@ -1,7 +1,7 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:fitness_app/src/services/authentication.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -11,29 +11,66 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-          Center(
-              child:
-                  Text(FirebaseAuth.instance.currentUser?.displayName ?? '')),
-          Center(
-              child: Text(
-                  FirebaseAuth.instance.currentUser?.emailVerified.toString() ??
-                      '')),
-          TextButton(
-              onPressed: () {
-                // FirebaseAuth.instance.signOut();
-                Provider.of<ApplicationStateNotifier>(context, listen: false)
-                    .signOut();
-              },
-              child: const Text('Log Out')),
-          TextButton(
-              onPressed: () {
-                FirebaseAuth.instance.currentUser?.sendEmailVerification();
-              },
-              child: const Text('Send Email Verification')),
-        ],
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            Container(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        greeting(),
+                        style: Theme.of(context).textTheme.headline4,
+                      ),
+                      Text(
+                        FirebaseAuth.instance.currentUser?.displayName ??
+                            'Guest',
+                        style: Theme.of(context).textTheme.headline5,
+                      ),
+                    ],
+                  ),
+                  CachedNetworkImage(
+                    imageUrl: FirebaseAuth.instance.currentUser?.photoURL ??
+                        'https://www.gravatar.com/avatar/00000000000000000000000000000000?d=mp&f=y',
+                    imageBuilder: (context, imageProvider) => Container(
+                      width: 150,
+                      height: 150,
+                      decoration: BoxDecoration(
+                        color: Colors.grey,
+                        shape: BoxShape.circle,
+                        image: DecorationImage(
+                          image: imageProvider,
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                    ),
+                    placeholder: (context, url) =>
+                        const CircularProgressIndicator(),
+                    errorWidget: (context, url, error) =>
+                        const Center(child: Icon(Icons.error)),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
+  }
+}
+
+String greeting() {
+  if (DateTime.now().hour < 12) {
+    return 'Good Morning,';
+  } else if (DateTime.now().hour < 16) {
+    return 'Good Afternoon,';
+  } else if (DateTime.now().hour < 22) {
+    return 'Good Evening,';
+  } else {
+    return 'Good Night,';
   }
 }
--- lib/src/theme/theme.dart
+++ lib/src/theme/theme.dart
@@ -4,7 +4,7 @@ import 'package:google_fonts/google_fonts.dart';
 class MyTheme {
   static String appName = 'aMAze';
   static ThemeData lightTheme() => ThemeData(
-        primarySwatch: primarySwatch,
+        primarySwatch: Colors.purple,
         textTheme: myTextTheme(),
         primaryTextTheme: myTextTheme(),
       );
@@ -27,25 +27,17 @@ class MyTheme {
   );
 
   static ThemeData darkTheme() => ThemeData(
-        primarySwatch: darkSwatch,
-        textTheme: myTextTheme(),
+        primarySwatch: Colors.green,
+        textTheme: const TextTheme(
+          bodyText1: TextStyle(),
+          bodyText2: TextStyle(),
+        ).apply(
+          bodyColor: Colors.white,
+          displayColor: Colors.white,
+        ),
         primaryTextTheme: myTextTheme(),
-        // scaffoldBackgroundColor: const Color(0xFF212121),
+        scaffoldBackgroundColor: const Color(0xFF212121),
       );
-  static const MaterialColor darkSwatch =
-      MaterialColor(0xFF212121, <int, Color>{
-    50: Color(0xFFFAFAFA),
-    100: Color(0xFFF5F5F5),
-    200: Color(0xFFEEEEEE),
-    300: Color(0xFFE0E0E0),
-    400: Color(0xFFBDBDBD),
-    500: Color(0xFF9E9E9E),
-    600: Color(0xFF757575),
-    700: Color(0xFF616161),
-    800: Color(0xFF424242),
-    900: Color(0xFF212121), // Primary Color
-    1000: Color(0xFFACACAC),
-  });
 
   // A function for textTheme
   static TextTheme myTextTheme() {
