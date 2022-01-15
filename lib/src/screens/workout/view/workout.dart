6da9138de343ba4357638f8a52426b1a6df16b65
YadavGulshan <yadavgulshan542@gmail.com>
2022-01-15 13:35:31 +0530

    Implemented db with workout page

--- lib/src/screens/workout/view/workout.dart
+++ lib/src/screens/workout/view/workout.dart
@@ -1,3 +1,5 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:fitness_app/src/components/view/common.dart';
 import 'package:flutter/material.dart';
 
 class WorkoutView extends StatelessWidget {
@@ -10,10 +12,52 @@ class WorkoutView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    CollectionReference workouts =
+        FirebaseFirestore.instance.collection('workouts');
     return Scaffold(
-      body: Center(
-        child: Text(id),
-      ),
-    );
+        body: FutureBuilder<QuerySnapshot>(
+      future: workouts.doc(id).collection('workout').get(),
+      builder: (context, snapshot) {
+        if (snapshot.hasError) {
+          return Center(
+            child: Text('Error: ${snapshot.error}'),
+          );
+        }
+        if (snapshot.hasData) {
+          var doc = snapshot.data!;
+          List<Widget> list = [];
+          for (var i = 0; i < doc.docs.length; i++) {
+            list.add(
+              ListTile(
+                title: Text(doc.docs[i]['name']),
+              ),
+            );
+          }
+          return Stack(
+            children: [
+              ListView(
+                children: list,
+              ),
+              // Start button here!
+              Align(
+                alignment: Alignment.bottomCenter,
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 16.0),
+                  child: StyledButton(
+                      child: Text(
+                        'START TRAINING',
+                        style: Theme.of(context).textTheme.headline6,
+                      ),
+                      onPressed: () {}),
+                ),
+              )
+            ],
+          );
+        }
+        return const Center(
+          child: CircularProgressIndicator(),
+        );
+      },
+    ));
   }
 }
